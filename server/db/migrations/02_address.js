0c5839b99f17899380f45738037b3bd27395836d
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-02-11 22:06:42 -0700

    adding new colomn

--- server/db/migrations/02_address.js
+++ server/db/migrations/02_address.js
@@ -1,13 +1,17 @@
+// TODO
+// need to change camel case to underscore 
+
 
 exports.up = function(knex) {
   return knex.schema.createTable('address', table => {
     table.increments();
-    table.integer('user_id').notNullable();
+    table.integer('user_id').references('users.id').notNullable().onDelete('CASCADE');
     table.string('stAddress').notNullable();
     table.string('optAddress');
     table.string('city').notNullable();
     table.string('state').notNullable();
     table.integer('zipCode').notNullable();
+    table.timestamps(true, true);
   })
 };
 
