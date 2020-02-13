c0f61a335c6f441313ffba167749e3011239913c
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-02-12 19:07:35 -0700

    updating reference

--- server/db/migrations/04_user_insurances.js
+++ server/db/migrations/04_user_insurances.js
@@ -2,8 +2,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('user_insurances', table => {
     table.increments();
-    table.integer('user_id').notNullable();
-    table.integer('insurance_company_id').references('insurance_company.id').notNullable().onDelete('CASCADE');
+    table.integer('user_id').notNullable().references('users.id').onDelete('CASCADE');
+    table.integer('insurance_company_id').notNullable();
     table.string('policy_num');
     table.string('policy_type');
     table.timestamps(true, true);
