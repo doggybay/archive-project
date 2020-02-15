2d1a881932003bc78a6e5a62c85029de16d82216
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-02-15 11:27:55 -0700

    working on display list

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import ArchiveItemsList from '../components/archive-items/ArchiveItemsList'
 
 const Dashboard = (props) => {
-
+  console.log('dash props:', props)
   return (
     <div>
       <h1>Dashboard!!!</h1>
+      <ArchiveItemsList />
     </div>
   )
 }
