7f7e39930ec5918775f2e28f8631f590cb69bfef
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-02-15 13:08:59 -0700

    working on individual item

---
+++ components/archive-items/ArchiveItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const ArchiveItem = (props) => {
+  console.log('props item page: ', props)
+  return (
+    <div>
+      <h1>Archive Item Page</h1>
+    </div>
+  )
+};
+
+
+export default ArchiveItem;
