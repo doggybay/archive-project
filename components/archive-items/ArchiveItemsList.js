2a36655ad1e51ed2d2a6a500ec73e6c3ac74cfdd
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-02-15 13:10:31 -0700

    working on icon link

--- components/archive-items/ArchiveItemsList.js
+++ components/archive-items/ArchiveItemsList.js
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import { Paper, Typography, Card, CardActionArea, CardActions, CardContent, CardMedia, IconButton, GridList, GridListTile, GridListTileBar } from '@material-ui/core';
 import InfoIcon from '@material-ui/icons/Info';
+import Link from '../../src/Link';
+import { useRouter } from 'next/router'
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -21,32 +24,38 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const ArchiveItemsList = () => {
+const ArchiveItemsList = (props) => {
   const user = useSelector(state => state.users.oneUser)
   const archiveItems = user ? user.archive_items : [];
+  const router = useRouter();
 
   const classes = useStyles();
 
   const listOfArchiveItems = archiveItems.map(item => {
     const pictures = item ? item.pictures : [];
-    
+    const type = item ? item.types : [];
 
-
+    console.log('list props: ', router)
 
     return (
       <GridListTile key={item.id} cols={1}>
         <img src={pictures[0] ? pictures[0].pic : ""} alt={item.make} />
         <GridListTileBar
-              actionIcon={
-                <IconButton aria-label={`info about ${item.make}`} className={classes.icon}>
-                  <InfoIcon />
-                </IconButton>
-              }
-            />
+          actionIcon={
+            <IconButton
+              aria-label={`info about ${type.name}`}
+              className={classes.icon}
+              component={Link}
+              href={`/archiveitem?id=${item.id}`}
+            >
+              <InfoIcon />
+            </IconButton>
+          }
+        />
       </GridListTile>
     );
   })
-  console.log('item list: ', archiveItems)
+  //console.log('item list: ', archiveItems)
 
   return (
     <div className={classes.root}>
