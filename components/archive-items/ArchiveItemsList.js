7132d43df03017e24897c0cb6faf2376918d6322
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-02-21 07:58:43 -0700

    working on archive items

--- components/archive-items/ArchiveItemsList.js
+++ components/archive-items/ArchiveItemsList.js
@@ -1,70 +1,123 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Typography, Card, CardActionArea, CardActions, CardContent, CardMedia, IconButton, GridList, GridListTile, GridListTileBar } from '@material-ui/core';
+import { Paper, Typography, Card, CardActionArea, CardActions, CardContent, CardMedia, IconButton, GridList, GridListTile, GridListTileBar, Button, Container, Grid } from '@material-ui/core';
 import InfoIcon from '@material-ui/icons/Info';
 import Link from '../../src/Link';
 import { useRouter } from 'next/router'
 
+import { fetchOneArchiveItem } from '../../store/archive-items/actionCreators'
 
-const useStyles = makeStyles(theme => ({
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     display: "flex",
+//     flexWrap: "wrap",
+//     justifyContent: "space-around",
+//     overflow: "hidden",
+//     backgroundColor: theme.palette.background.paper
+//   },
+//   gridList: {
+//     width: 700,
+//     height: "auto"
+//   },
+//   icon: {
+//     color: "rgba(255, 255, 255, 0.54)"
+//   }
+// }));
+
+const useStyles = makeStyles({
   root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden",
-    backgroundColor: theme.palette.background.paper
-  },
-  gridList: {
-    width: 700,
-    height: "auto"
-  },
-  icon: {
-    color: "rgba(255, 255, 255, 0.54)"
+    margin: "5px",
+    width: "600px",
+    minHeight: "200px",
+    "&:hover": {
+      boxShadow: "-1px 5px 29px 0px rgba(0,0,0,0.5)"
+    }
   }
-}));
+});
 
 const ArchiveItemsList = (props) => {
   const user = useSelector(state => state.users.oneUser)
   const archiveItems = user ? user.archive_items : [];
   const router = useRouter();
+  const dispatch = useDispatch();
 
+  const getOneArchiveItem = (id) => {
+    dispatch(fetchOneArchiveItem(id))
+    
+  }
   const classes = useStyles();
 
   const listOfArchiveItems = archiveItems.map(item => {
     const pictures = item ? item.pictures : [];
     const type = item ? item.types : [];
 
-    console.log('list props: ', router)
+    
 
     return (
-      <GridListTile key={item.id} cols={1}>
-        <img src={pictures[0] ? pictures[0].pic : ""} alt={item.make} />
-        <GridListTileBar
-          actionIcon={
-            <IconButton
-              aria-label={`info about ${type.name}`}
-              className={classes.icon}
-              component={Link}
-              href={`/archiveitem?id=${item.id}`}
-            >
-              <InfoIcon />
-            </IconButton>
-          }
+      <Card className={classes.root} style={{ marginTop: "15px" }}>
+      <CardActionArea onClick={() => getOneArchiveItem(item.id)}>
+      <Link href={`/archiveitem?id=${item.id}`} >
+        <CardMedia
+        component="img"
+        alt="Contemplative Reptile"
+        height="140"
+        image={pictures[0] ? pictures[0].pic : ""}
+        title="Contemplative Reptile"
         />
-      </GridListTile>
+        </Link>
+        </CardActionArea>
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+        {item.make}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+        {item.description}
+        </Typography>
+        </CardContent>
+      </Card>
     );
   })
   //console.log('item list: ', archiveItems)
 
   return (
-    <div className={classes.root}>
-      <GridList cellHeight={200} className={classes.gridList} cols={3}>
-        {listOfArchiveItems}
-        
-      </GridList>
-    </div>
+    <Grid
+      container
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "10px",
+        marginBottom: "0px"
+      }}
+    >
+      {listOfArchiveItems}
+    </Grid>
   );
 }
 
 export default ArchiveItemsList;
+
+
+
+
+// <GridList cellHeight={200} className={classes.gridList} cols={3}>
+//   {listOfArchiveItems}
+// </GridList>;
+
+
+// <GridListTile key={item.id} cols={1}>
+//         <img src={pictures[0] ? pictures[0].pic : ""} alt={item.make} />
+//         <GridListTileBar
+//           actionIcon={
+//             <IconButton
+//               aria-label={`info about ${type.name}`}
+//               className={classes.icon}
+//               component={Link}
+//               href={`/archiveitem?id=${item.id}`}
+//               onClick={() => getOneArchiveItem(item.id)}
+//             >
+//               <InfoIcon />
+//             </IconButton>
+//           }
+//         />
+//       </GridListTile>
--- pages/ArchiveItem.js
+++ pages/ArchiveItem.js
@@ -2,15 +2,51 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import { useRouter } from 'next/router'
+import { Container, Paper, Typography, Grid, Card, CardActionArea, CardActions, CardContent, CardMedia } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  grid: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "10px",
+    marginBottom: "0px"
+  },
+  root: {
+    minWidth: 345
+  },
+  media: {
+    height: 140
+  }
+});
 
 const ArchiveItem = (props) => {
+  const archiveItem = useSelector(state => state.archiveItems.oneArchiveItem)
   const router = useRouter();
-  console.log('props item page: ', router)
+  const classes = useStyles();
+  const pictures = archiveItem.pictures
+  const type = archiveItem.types
+
+  const listOfPics = pictures.map(picture => {
+    console.log('l',type)
+    return (
+      <Card key={picture.id} className={classes.root} style={{ marginRight: "15px", marginBottom: "15px"}}>
+      <CardMedia
+      className={classes.media}
+      image={picture.pic}
+      title="Contemplative Reptile"
+      />
+      
+        
+      </Card>
+    );
+  })
+
   return (
-    <div>
-      <h1>Archive Item Page</h1>
-    </div>
-  )
+    <Grid container className={classes.grid}>
+      
+      {listOfPics}
+    </Grid>
+  );
 };
 
 
