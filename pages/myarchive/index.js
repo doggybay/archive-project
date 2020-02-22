5397700d06b765d704dd0db2401db88e8a295643
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-02-22 10:18:56 -0700

    working on structure

---
+++ pages/myarchive/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ArchiveItemsList from '../../components/archive-items/ArchiveItemsList';
+
+const MyArchive = () => {
+
+  return (
+    <div>
+      <h1>My Archive</h1>
+      <ArchiveItemsList />
+    </div>
+  );
+}
+
+export default MyArchive
