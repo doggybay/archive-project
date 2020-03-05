b4006ec121d8ea10066e65e0f13e4fca03e9c1c3
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-03-04 22:52:24 -0700

    adding edit route

--- server/routes/archiveItems.js
+++ server/routes/archiveItems.js
@@ -6,5 +6,6 @@ const archiveItemsController = require('../controllers/archiveItems')
 router.get('/archive-items', archiveItemsController.getAllArchiveItems);
 router.get('/archive-items/:id', archiveItemsController.getOneArchiveItem);
 router.post('/archive-items', archiveItemsController.addArchiveItem);
+router.patch('/archive-items/:id', archiveItemsController.editArchiveItem)
 
 module.exports = router;
