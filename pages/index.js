0dc5dab6971b63c1c4f509fd87e8043216a7faf0
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-03-10 07:14:23 -0700

    working on user login

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,15 @@ import { useDispatch } from 'react-redux';
 import { Container, Typography, Box } from '@material-ui/core';
 import MuiLink from '@material-ui/core/Link';
 import ProTip from '../src/ProTip';
-import Link from '../src/Link';import CssBaseline from '@material-ui/core/CssBaseline';
+import Link from '../src/Link';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 
 
 import { fetchAllUsers, fetchOneUser } from '../src/store/users/actionCreators';
 import { fetchAllArchiveItems } from '../src/store/archive-items/actionCreators';
 import { fetchAllTypes } from '../src/store/types/actionCreators';
-
+import Login from '../src/components/auth/Login'
 import ArchiveItem from '../pages/archiveitem';
 
 
@@ -25,15 +26,17 @@ export default function App(props) {
   
   return (
     <Fragment>
-      <Container maxWidth="sm">
-        <Box my={10}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Welcome to Archive
-          </Typography>
-          
-        </Box>
-      </Container>
+      <Login />
       
     </Fragment>
   );
 }
+
+
+// <Container maxWidth="sm">
+//   <Box my={10}>
+//     <Typography variant="h4" component="h1" gutterBottom>
+//       Welcome to Archive
+//     </Typography>
+//   </Box>
+// </Container>;
