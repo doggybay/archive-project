6c277f1bac0e787e7d8b527659af9ce92b800f4a
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-03-11 06:14:53 -0700

    clean up

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,12 @@ export default function App(props) {
 const [localUser, setLocalUser] = useState({});
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("loggedInUser"));
-    setLocalUser(data.hasOwnProperty('id') ? data : {});
     
   }, [])
 
-  console.log("app: ", localUser);
   return (
     <Fragment>
-      <Login localUser={localUser} />
+      <Login  />
       
     </Fragment>
   );
